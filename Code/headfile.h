b255408082afadfa21a62738531bc14edcc42925
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2025-06-07 07:07:40 +0000

    added basic stuff

---
+++ Code/headfile.h
@@ -0,0 +1,21 @@
+#ifndef HEADFILE_H
+#define HEADFILE_H
+#include <iostream>
+#include <list>
+using namespace std;
+
+class airport {
+    string city;
+    int flight_capacity;
+    
+    public:
+
+};
+
+class graph {
+
+    public:
+
+};
+
+#endif
