2396d9b608631e194b6168f8fe2547180ef869fd
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2025-06-07 00:20:23 -0700

    Update headfile.h

--- Code/headfile.h
+++ Code/headfile.h
@@ -2,18 +2,23 @@
 #define HEADFILE_H
 #include <iostream>
 #include <list>
+#include <vector>
 using namespace std;
 
 class airport {
     string city;
     int flight_capacity;
-    
+    airport* next;
     public:
-
+    airport(): city("n/a"), flight_capacity(0){}
+    string get_city() { return this->city; }
+    int get_flightCap() { return this->flight_capacity; }
 };
 
 class graph {
-
+    int v; //num vertices
+    list<int>* grid;
+    //airport* arr[]
     public:
 
 };
